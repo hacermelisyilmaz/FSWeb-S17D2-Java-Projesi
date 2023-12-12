cad76d3cb370a7158d1839564e5a365d90399bee
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-12 16:45:25 +0300

    Autowired constructor and PostConstruct

--- src/main/java/com/workintech/spring/developer/rest/DeveloperController.java
+++ src/main/java/com/workintech/spring/developer/rest/DeveloperController.java
@@ -1,6 +1,11 @@
 package com.workintech.spring.developer.rest;
 
 import com.workintech.spring.developer.model.Developer;
+import com.workintech.spring.developer.tax.DeveloperTax;
+import com.workintech.spring.developer.tax.Taxable;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +15,16 @@ import java.util.Map;
 @RestController
 @RequestMapping("/developer")
 public class DeveloperController {
-    Map<Integer, Developer> developers = new HashMap<>();
+    private Taxable developerTax;
+    Map<Integer, Developer> developers;
+
+    @Autowired
+    public DeveloperController(@Qualifier Taxable developerTax) {
+        this.developerTax = developerTax;
+    }
+
+    @PostConstruct
+    public void initializeDevelopersMap() {
+        developers = new HashMap<>();
+    }
 }
