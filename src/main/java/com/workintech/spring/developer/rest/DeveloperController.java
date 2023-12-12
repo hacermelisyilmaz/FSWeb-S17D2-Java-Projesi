f54c101edbee8cb9aa36eb17fb57899b2fcb2db0
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-12 14:38:00 +0300

    developers Map defined

--- src/main/java/com/workintech/spring/developer/rest/DeveloperController.java
+++ src/main/java/com/workintech/spring/developer/rest/DeveloperController.java
@@ -1,9 +1,14 @@
 package com.workintech.spring.developer.rest;
 
+import com.workintech.spring.developer.model.Developer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/developer")
 public class DeveloperController {
+    Map<Integer, Developer> developers = new HashMap<>();
 }
