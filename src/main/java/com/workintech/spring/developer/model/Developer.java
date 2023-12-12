892b7946c6a6a35f39d6d92ea9c9e634096657e2
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-12 16:56:33 +0300

    Salaries converted to double from int

--- src/main/java/com/workintech/spring/developer/model/Developer.java
+++ src/main/java/com/workintech/spring/developer/model/Developer.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component;
 public class Developer {
     private int id;
     private String name;
-    private int salary;
+    private double salary;
     private Experience experience;
 
-    public Developer(int id, String name, int salary, Experience experience) {
+    public Developer(int id, String name, double salary, Experience experience) {
         this.id = id;
         this.name = name;
         this.salary = salary;
@@ -33,7 +33,7 @@ public class Developer {
         this.name = name;
     }
 
-    public int getSalary() {
+    public double getSalary() {
         return salary;
     }
 
