b75d8d129aea620aa69c0dac75fa88a9f88f886e
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-12 16:47:37 +0300

    Get request with getDevelopers

--- src/main/java/com/workintech/spring/developer/rest/DeveloperController.java
+++ src/main/java/com/workintech/spring/developer/rest/DeveloperController.java
@@ -1,19 +1,20 @@
 package com.workintech.spring.developer.rest;
 
 import com.workintech.spring.developer.model.Developer;
-import com.workintech.spring.developer.tax.DeveloperTax;
 import com.workintech.spring.developer.tax.Taxable;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/developer")
+@RequestMapping("workintech/developers")
 public class DeveloperController {
     private Taxable developerTax;
     Map<Integer, Developer> developers;
@@ -26,5 +27,10 @@ public class DeveloperController {
     @PostConstruct
     public void initializeDevelopersMap() {
         developers = new HashMap<>();
+    }
+
+    @GetMapping
+    public List getDevelopers() {
+        return developers.values().stream().toList();
     }
 }
