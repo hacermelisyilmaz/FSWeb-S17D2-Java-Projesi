cf0072232cab70d46b9db8e956d3bcccf7dc9bb0
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-12 14:15:01 +0300

    Developer class created with constructor, getters & setters

---
+++ src/main/java/com/workintech/spring/developer/model/Developer.java
@@ -0,0 +1,51 @@
+package com.workintech.spring.developer.model;
+
+import com.workintech.spring.developer.enums.Experience;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Developer {
+    private int id;
+    private String name;
+    private int salary;
+    private Experience experience;
+
+    public Developer(int id, String name, int salary, Experience experience) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.experience = experience;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Experience getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Experience experience) {
+        this.experience = experience;
+    }
+}
